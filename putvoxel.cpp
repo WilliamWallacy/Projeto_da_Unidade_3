17d3e8ccd56910e30c68c7b4866ca4413545b4ea
William Wallacy <113632966+WilliamWallacy@users.noreply.github.com>
2022-12-21 22:31:59 -0300

    Create putvoxel.cpp

---
+++ putvoxel.cpp
@@ -0,0 +1,17 @@
+#include "putvoxel.h"
+#include <math.h>
+
+PutVoxel:: PutVoxel(int x, int y, int z, float r, float g, float b, float a){
+    this -> x = x;
+    this -> y = y;
+    this -> z = z;
+    this -> r = r;
+    this -> g = g;
+    this -> b = b;
+    this -> a = a;
+  }
+
+   void PutVoxel::draw (Sculptor &s){
+      s.setColor(r, g, b, a);//seleciona as cores
+      s.putVoxel(x,y,z);
+  }
